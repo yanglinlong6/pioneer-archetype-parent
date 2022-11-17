185f5cf30277cc3c0e65dacfd9482c9239da70e8
杨林龙 <yangll@didihu.com.cn>
2022-11-17 17:53:02 +0800

    优化kafka生产者模板发送消息配置

--- pioneer-rest-admin-kafka-archetype/pioneer-rest-admin-kafka-producer/src/main/java/cn/com/glsx/kafka/producer/modules/producer/TestProducer.java
+++ pioneer-rest-admin-kafka-archetype/pioneer-rest-admin-kafka-producer/src/main/java/cn/com/glsx/kafka/producer/modules/producer/TestProducer.java
@@ -1,20 +1,25 @@
 package cn.com.glsx.kafka.producer.modules.producer;
 
+import kafka.javaapi.producer.Producer;
+import kafka.producer.KeyedMessage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 /**
  * @author payu
  */
+@Slf4j
 @Component
 public class TestProducer {
 
     @Autowired
-    private KafkaTemplate kafkaTemplate;
+    private Producer producer;
 
     public void sendMsg(String message) {
-        kafkaTemplate.send("test_topic", message);
+        KeyedMessage<byte[], byte[]> messageBody = new KeyedMessage<>("test_topic", message.getBytes(), message.getBytes());
+        producer.send(messageBody);
+        log.info(message);
     }
 
 }
